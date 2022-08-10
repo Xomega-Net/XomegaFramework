666a5bb37d105f652a06cc66eedca73a068803b2
Xomega Team <misc@xomega.net>
2022-08-10 00:41:18 -0400

    Added BigIntegerProperty, PositiveBigIntProperty and BigIntegerKeyProperty

--- src/Xomega.Framework/DataProperties/IntegerProperty.cs
+++ src/Xomega.Framework/DataProperties/IntegerProperty.cs
@@ -8,30 +8,30 @@ using System.Globalization;
 namespace Xomega.Framework.Properties
 {
     /// <summary>
-    /// A data property that holds integer values.
+    /// A data property that holds big integer values (long).
     /// </summary>
-    public class IntegerProperty : DataProperty<int?>
+    public class BigIntegerProperty : DataProperty<long?>
     {
         /// <summary>
         /// The minimum valid value for the property.
         /// </summary>
-        public int MinimumValue { get; set; }
+        public long MinimumValue { get; set; }
 
         /// <summary>
         /// The maximum valid value for the property.
         /// </summary>
-        public int MaximumValue { get; set; }
+        public long MaximumValue { get; set; }
 
         /// <summary>
-        ///  Constructs an IntegerProperty with a given name and adds it to the parent data object under this name.
+        ///  Constructs a BigIntegerProperty with a given name and adds it to the parent data object under this name.
         /// </summary>
         /// <param name="parent">The parent data object to add the property to if applicable.</param>
         /// <param name="name">The property name that should be unique within the parent data object.</param>
-        public IntegerProperty(DataObject parent, string name)
+        public BigIntegerProperty(DataObject parent, string name)
             : base(parent, name)
         {
-            MinimumValue = int.MinValue;
-            MaximumValue = int.MaxValue;
+            MinimumValue = long.MinValue;
+            MaximumValue = long.MaxValue;
 
             Validator += ValidateInteger;
             Validator += ValidateMinimum;
@@ -39,19 +39,19 @@ namespace Xomega.Framework.Properties
         }
 
         /// <summary>
-        /// Overrides the base method to construct a list of non-Nullable integer values
+        /// Overrides the base method to construct a list of non-Nullable long values
         /// for the Transport format, since it's a value type.
         /// </summary>
         /// <param name="format">The format to create a new list for.</param>
         /// <returns>A new list for the given format.</returns>
         protected override IList CreateList(ValueFormat format)
         {
-            return format == ValueFormat.Transport ? new List<int>() : base.CreateList(format);
+            return format == ValueFormat.Transport ? new List<long>() : base.CreateList(format);
         }
 
         /// <summary>
         /// Converts a single value to a given format. For typed formats
-        /// this method tries to convert various types of values to a nullable integer.
+        /// this method tries to convert various types of values to a nullable long.
         /// </summary>
         /// <param name="value">A single value to convert to the given format.</param>
         /// <param name="format">The value format to convert the value to.</param>
@@ -60,19 +60,18 @@ namespace Xomega.Framework.Properties
         {
             if (format.IsTyped())
             {
-                if (value is int?) return value;
-                if (value is int) return (int?)value;
-                if (value is double) return (int?)((double)value);
+                if (value is long?) return value;
+                if (value is long) return (long?)value;
+                if (value is double d) return (long?)d;
                 if (IsValueNull(value, format)) return null;
-                int i;
-                if (int.TryParse(Convert.ToString(value), NumberStyles.Number, null, out i)) return i;
+                if (long.TryParse(Convert.ToString(value), NumberStyles.Number, null, out long i)) return i;
                 if (format == ValueFormat.Transport) return null;
             }
             return base.ConvertValue(value, format);
         }
 
         /// <summary>
-        /// A validation function that checks if the value is an integer and reports a validation error if not.
+        /// A validation function that checks if the value is a long and reports a validation error if not.
         /// </summary>
         /// <param name="dp">Data property being validated.</param>
         /// <param name="value">The value to validate.</param>
@@ -80,12 +79,12 @@ namespace Xomega.Framework.Properties
         public static void ValidateInteger(DataProperty dp, object value, DataRow row)
         {
             if (dp != null && !dp.IsValueNull(value, ValueFormat.Internal)
-                && !(value is int) && !(value is short) && !(value is byte))
+                && !(value is long) && !(value is int) && !(value is short) && !(value is byte))
                 dp.AddValidationError(row, Messages.Validation_IntegerFormat, dp);
         }
 
         /// <summary>
-        /// A validation function that checks if the value is an integer that is not less
+        /// A validation function that checks if the value is a long that is not less
         /// than the property minimum and reports a validation error if it is.
         /// </summary>
         /// <param name="dp">Data property being validated.</param>
@@ -93,7 +92,7 @@ namespace Xomega.Framework.Properties
         /// <param name="row">The row in a list object or null for regular data objects.</param>
         public static void ValidateMinimum(DataProperty dp, object value, DataRow row)
         {
-            if (dp is IntegerProperty idp && (value is int?) && ((int?)value).Value < idp.MinimumValue)
+            if (dp is BigIntegerProperty idp && (value is long?) && ((long?)value).Value < idp.MinimumValue)
                 dp.AddValidationError(row, Messages.Validation_NumberMinimum, dp, idp.MinimumValue);
         }
 
@@ -106,8 +105,70 @@ namespace Xomega.Framework.Properties
         /// <param name="row">The row in a list object or null for regular data objects.</param>
         public static void ValidateMaximum(DataProperty dp, object value, DataRow row)
         {
-            if (dp is IntegerProperty idp && (value is int?) && ((int?)value).Value > idp.MaximumValue)
+            if (dp is BigIntegerProperty idp && (value is long?) && ((long?)value).Value > idp.MaximumValue)
                 dp.AddValidationError(row, Messages.Validation_NumberMaximum, dp, idp.MaximumValue);
+        }
+    }
+
+    /// <summary>
+    /// An big integer property for positive numbers only.
+    /// </summary>
+    public class PositiveBigIntProperty : BigIntegerProperty
+    {
+        /// <summary>
+        ///  Constructs a PositiveBigIntProperty with a given name and adds it to the parent data object under this name.
+        /// </summary>
+        /// <param name="parent">The parent data object to add the property to if applicable.</param>
+        /// <param name="name">The property name that should be unique within the parent data object.</param>
+        public PositiveBigIntProperty(DataObject parent, string name)
+            : base(parent, name)
+        {
+            MinimumValue = 1;
+        }
+    }
+
+    /// <summary>
+    /// An integer property for numbers in the range defined by the <c>int</c> CLR type.
+    /// The <c>int</c> type is used for the transport value format.
+    /// </summary>
+    public class IntegerProperty : BigIntegerProperty
+    {
+        /// <summary>
+        ///  Constructs a BigIntegerProperty with a given name and adds it to the parent data object under this name.
+        /// </summary>
+        /// <param name="parent">The parent data object to add the property to if applicable.</param>
+        /// <param name="name">The property name that should be unique within the parent data object.</param>
+
+        public IntegerProperty(DataObject parent, string name)
+            : base(parent, name)
+        {
+            MinimumValue = int.MinValue;
+            MaximumValue = int.MaxValue;
+        }
+
+        /// <summary>
+        /// Overrides the base method to construct a list of non-Nullable int values
+        /// for the Transport format.
+        /// </summary>
+        /// <param name="format">The format to create a new list for.</param>
+        /// <returns>A new list for the given format.</returns>
+        protected override IList CreateList(ValueFormat format)
+        {
+            return format == ValueFormat.Transport ? new List<int>() : base.CreateList(format);
+        }
+
+        /// <summary>
+        /// Converts a single value to a given format. For the transport format
+        /// the value is converted to an <c>int</c> type.
+        /// </summary>
+        /// <param name="value">A single value to convert to the given format.</param>
+        /// <param name="format">The value format to convert the value to.</param>
+        /// <returns>The value converted to the given format.</returns>
+        protected override object ConvertValue(object value, ValueFormat format)
+        {
+            if (format == ValueFormat.Transport && value is int?)
+                return (int?)value;
+            return base.ConvertValue(value, format);
         }
     }
 
@@ -235,7 +296,25 @@ namespace Xomega.Framework.Properties
     }
 
     /// <summary>
-    /// An integer key property to distinguish keys from regular integers. Unlike for regular integers 
+    /// An big integer key property to distinguish keys from regular big integers. Unlike for regular long values, 
+    /// comparison operators other than equality may not be applicable to big integer keys.
+    /// </summary>
+    public class BigIntegerKeyProperty : BigIntegerProperty
+    {
+        /// <summary>
+        ///  Constructs a BigIntKeyProperty with a given name and adds it to the parent data object under this name.
+        /// </summary>
+        /// <param name="parent">The parent data object to add the property to if applicable.</param>
+        /// <param name="name">The property name that should be unique within the parent data object.</param>
+
+        public BigIntegerKeyProperty(DataObject parent, string name)
+            : base(parent, name)
+        {
+        }
+    }
+
+    /// <summary>
+    /// An integer key property to distinguish keys from regular integers. Unlike for regular integers, 
     /// comparison operators other than equality may not be applicable to integer keys.
     /// </summary>
     public class IntegerKeyProperty : IntegerProperty
