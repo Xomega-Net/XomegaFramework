ff156eab0dd3826483c05a150023fcb9f1729802
Xomega Team <misc@xomega.net>
2022-12-23 00:31:36 -0500

    Fixes and updates to Syncfusion selection controls

--- src/Xomega.Syncfusion.Blazor/Controls/XSfOptionsComponent.cs
+++ src/Xomega.Syncfusion.Blazor/Controls/XSfOptionsComponent.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.Components;
 using Syncfusion.Blazor.DropDowns;
 using Syncfusion.Blazor.Inputs;
 using Syncfusion.Blazor.Popups;
-using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xomega.Framework;
 using Xomega.Framework.Blazor;
+using Xomega.Framework.Lookup;
 
 namespace Xomega._Syncfusion.Blazor
 {
@@ -20,7 +22,12 @@ namespace Xomega._Syncfusion.Blazor
         /// <summary>
         /// Gets the type of float label to use, if any.
         /// </summary>
-        public FloatLabelType FloatLabelType => ShowLabel ? FloatLabelType.Always : FloatLabelType.Never;
+        public FloatLabelType FloatLabelType => ShowLabel ? FloatLabel ? FloatLabelType.Auto : FloatLabelType.Always : FloatLabelType.Never;
+
+        /// <summary>
+        /// Indicates whether the label floats above the control on focus or always stays above the control.
+        /// </summary>
+        [Parameter] public bool FloatLabel { get; set; }
 
         /// <summary>
         /// Allows setting tooltip position for the validation error tooltip.
@@ -44,6 +51,16 @@ namespace Xomega._Syncfusion.Blazor
         [Parameter] public bool AllowFiltering { get; set; }
 
         /// <summary>
+        /// Custom template for displaying the value.
+        /// </summary>
+        [Parameter] public RenderFragment<T> ValueDisplayTemplate { get; set; }
+
+        /// <summary>
+        /// Actual template for displaying the value, which could be custom or default to the item template.
+        /// </summary>
+        public RenderFragment<T> ValueTemplate => ValueDisplayTemplate ?? Template;
+
+        /// <summary>
         /// The attribute of the option items to group the items by.
         /// </summary>
         [Parameter] public string GroupByAttribute { get; set; }
@@ -52,6 +69,18 @@ namespace Xomega._Syncfusion.Blazor
         /// A template to use for displaying group headers, when grouping is enabled.
         /// </summary>
         [Parameter] public RenderFragment<ComposedItemModel<T>> GroupTemplate { get; set; }
+
+        /// <summary>
+        /// Whether component allows custom values that are not in the lookup table
+        /// </summary>
+        [Parameter] public bool? AllowCustom { get; set; }
+
+        /// <summary>
+        /// Effective flag on whether component allows custom values that defaults to the property lookup validation configuration
+        /// </summary>
+        protected bool AllowCustomValue => AllowCustom != null ? AllowCustom.Value :
+                                           Property?.LookupValidation == LookupValidationType.None;
+
 
         /// <inheritdoc/>
         protected override void OnInitialized()
@@ -65,9 +94,6 @@ namespace Xomega._Syncfusion.Blazor
         {
             await base.OnParametersSetAsync();
             await GetAvailableItems(null);
-
-            if (ItemDisplayTemplate == null)
-                ItemDisplayTemplate = RenderValue;
 
             if (!string.IsNullOrEmpty(GroupByAttribute) && GroupTemplate == null)
                 GroupTemplate = RenderGroup;
@@ -106,9 +132,36 @@ namespace Xomega._Syncfusion.Blazor
         /// </summary>
         /// <typeparam name="TValue">The type of the item.</typeparam>
         /// <param name="args">Change event arguments.</param>
-        protected async Task OnValueChanged<TValue>(ChangeEventArgs<TValue, T> args)
+        protected virtual async Task OnValueChanged<TValue>(ChangeEventArgs<TValue, T> args)
         {
-            await UpdatePropertyAsync(args.ItemData);
+            // update property only from the user update, since SF would often send null value on its own here
+            if (ShouldUpdateValue(args))
+            {
+                await UpdatePropertyAsync(args.ItemData);
+            }
+        }
+
+        /// <summary>
+        /// Determines when the component should update the property value when control's value changes
+        /// </summary>
+        /// <typeparam name="TValue">Value type</typeparam>
+        /// <param name="args">Change event args</param>
+        /// <returns>True to update the value, false otherwise.</returns>
+        protected virtual bool ShouldUpdateValue<TValue>(ChangeEventArgs<TValue, T> args) => args.IsInteracted;
+
+        /// <summary>
+        /// Async handler for filtering the list using the property's AsyncItemsProvider.
+        /// </summary>
+        /// <param name="args">Arguments that provide the query and return results.</param>
+        protected async Task OnActionComplete(ActionCompleteEventArgs<T> args)
+        {
+            if (AllowFiltering && Property?.AsyncItemsProvider != null)
+            {
+                string text = args.Query?.Queries?.Where?.First()?.value?.ToString();
+                var items = await Property.AsyncItemsProvider(text, Row, CancellationToken.None);
+                if (items != null)
+                    args.Result = items.Cast<T>();
+            }
         }
 
         /// <summary>
