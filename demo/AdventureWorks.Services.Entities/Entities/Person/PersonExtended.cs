4ef0f3a0dc79d03976a9a34156042a93a616db4d
XomegaNet <misc@xomega.net>
2023-06-07 00:40:50 -0400

    removing the demo project

--- demo/AdventureWorks.Services.Entities/Entities/Person/PersonExtended.cs
+++
@@ -1,8 +0,0 @@
-
-namespace AdventureWorks.Services.Entities
-{
-    public partial class Person
-    {
-        public string FullName => $"{LastName}, {FirstName}";
-    }
-}
--- demo/AdventureWorks.Services.Entities/Entities/Sales/CurrencyRateExtended.cs
+++
@@ -1,8 +0,0 @@
-
-namespace AdventureWorks.Services.Entities
-{
-    public partial class CurrencyRate
-    {
-        public string RateString => $"$1 = {EndOfDayRate} {ToCurrencyCodeObject.CurrencyCode}";
-    }
-}
